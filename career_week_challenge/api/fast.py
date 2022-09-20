d5c449dc326f07569e13a0ba2b8ed64c88abed03
william mwine <williammwine219@gmail.com>
2022-09-20 17:25:59 +0200

    api is ready locally , next step is deployment using docker and gcp

---
+++ career_week_challenge/api/fast.py
@@ -0,0 +1,32 @@
+import csv
+import codecs
+import pandas as pd
+from io import StringIO
+from typing import Union
+from fastapi import FastAPI,  UploadFile
+from fastapi.responses import FileResponse
+from career_week_challenge.interface.main import pred
+app = FastAPI()
+
+@app.get('/')
+def index():
+    return "Your api is local for now"
+
+@app.post("/upload/")
+def upload_file(file:  Union[UploadFile, None] = None):
+    if not file:
+        return FileResponse('raw_data/predicted.csv')
+    else:
+        #read bytes data
+        data = file.file.read()
+        #turn bytes into string
+        #TODO find more efficient way to d this
+        s=str(data,'utf-8')
+        #turn it into data frame
+        pred_data = pd.read_csv(StringIO(s))
+        print(f'#################### {type(pred_data)}##############################')
+
+        pred(pred_data)
+        print(f'#################### PREDICTIONS SAVED ##############################')
+
+        return FileResponse('raw_data/predicted.csv')
